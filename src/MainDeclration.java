8c15f966e46aa6c64a63a6e1f64309ab3ab7fc24
msapna717 <46435843+msapna717@users.noreply.github.com>
2019-10-09 12:24:35 +0530

    Update MainDeclration.java

--- src/MainDeclration.java
+++ src/MainDeclration.java
@@ -2,7 +2,7 @@
 public class MainDeclration {
 
 	public static void main(String[] args) {
-		Overriding obj1 = new Overriding();// with parent object you can not call only parent methods
+		Overriding obj1 = new Overriding();// with parent object you can call only parent methods
 		Child obj2 = new Child();// with child object you can call both parent and child methods
 		Overriding obj3 = new Child();// Run time polymorphism
 		obj3.f(15, 10);
